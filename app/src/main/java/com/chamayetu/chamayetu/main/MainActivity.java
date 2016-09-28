a93952349bb3812b017ed16c44647e4edb394283
BrianLusina <lusinabrian@gmail.com>
2016-09-28 10:40:45 +0300

    Update main content

--- app/src/main/java/com/chamayetu/chamayetu/main/MainActivity.java
+++ app/src/main/java/com/chamayetu/chamayetu/main/MainActivity.java
@@ -15,8 +15,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_activity);
-/*        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);*/
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action",
