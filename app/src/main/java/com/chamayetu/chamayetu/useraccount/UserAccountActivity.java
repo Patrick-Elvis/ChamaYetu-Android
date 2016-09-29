7963170922447e6cc18e4a34c9e4e8725c24a17f
BrianLusina <lusinabrian@gmail.com>
2016-09-29 11:51:54 +0300

    Add CardViews for user account

--- app/src/main/java/com/chamayetu/chamayetu/useraccount/UserAccountActivity.java
+++ app/src/main/java/com/chamayetu/chamayetu/useraccount/UserAccountActivity.java
@@ -4,22 +4,34 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.chamayetu.chamayetu.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class UserAccountActivity extends AppCompatActivity {
+    @BindView(R.id.useracct_profilecard) CardView profileCardView;
+    @BindView(R.id.useracct_chamacard) CardView chamaCardView;
+    @BindView(R.id.useracct_displayname) TextView userDisplayName;
+    @BindView(R.id.useracct_email) TextView userEmail;
+    @BindView(R.id.user_toolbar) Toolbar toolbar;
+    @BindView(R.id.fab) FloatingActionButton fab;
+    @BindView(R.id.useracct_chamarecycler) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.useracctact_layout);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.user_toolbar);
+        ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
         //TODO: change user profile in Firebase
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(view -> Snackbar.make(view,
                 "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
