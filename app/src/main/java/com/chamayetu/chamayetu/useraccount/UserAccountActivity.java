c2774d1171fe4d64695b3191b38f789c195b1782
BrianLusina <lusinabrian@gmail.com>
2016-09-29 11:57:50 +0300

    Init views into UIviews for User Profile

--- app/src/main/java/com/chamayetu/chamayetu/useraccount/UserAccountActivity.java
+++ app/src/main/java/com/chamayetu/chamayetu/useraccount/UserAccountActivity.java
@@ -10,19 +10,26 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
 import com.chamayetu.chamayetu.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class UserAccountActivity extends AppCompatActivity {
     @BindView(R.id.useracct_profilecard) CardView profileCardView;
+    @BindView(R.id.useracct_img) CircleImageView userImagView;
     @BindView(R.id.useracct_chamacard) CardView chamaCardView;
     @BindView(R.id.useracct_displayname) TextView userDisplayName;
     @BindView(R.id.useracct_email) TextView userEmail;
     @BindView(R.id.user_toolbar) Toolbar toolbar;
     @BindView(R.id.fab) FloatingActionButton fab;
     @BindView(R.id.useracct_chamarecycler) RecyclerView mRecyclerView;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseUser mFirebaseUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,9 +38,21 @@ public class UserAccountActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
 
-        //TODO: change user profile in Firebase
+        // Initialize Firebase Auth
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseUser = mFirebaseAuth.getCurrentUser();
+        initDataIntoViews();
+
+        //TODO: change user profile in Firebase, display a dialog
         fab.setOnClickListener(view -> Snackbar.make(view,
                 "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show());
+    }
+
+    /**Fetches data from Firebase,loads them into the views*/
+    private void initDataIntoViews() {
+        Glide.with(this).load(mFirebaseUser.getPhotoUrl()).into(userImagView);
+        userDisplayName.setText(mFirebaseUser.getDisplayName());
+        userEmail.setText(mFirebaseUser.getEmail());
     }
 }
