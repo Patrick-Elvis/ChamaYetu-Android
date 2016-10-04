06bb07e7b086154018f44ec0e506830c6ac2bf18
BrianLusina <lusinabrian@gmail.com>
2016-10-04 09:25:48 +0300

    Add UserRegistrationPolicy breaking

--- app/src/main/java/com/chamayetu/chamayetu/register/RegisterStepOne.java
+++ app/src/main/java/com/chamayetu/chamayetu/register/RegisterStepOne.java
@@ -1,6 +1,8 @@
 package com.chamayetu.chamayetu.register;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.annotation.ColorInt;
 import android.support.annotation.Nullable;
 import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
@@ -9,8 +11,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.chamayetu.chamayetu.R;
+import com.github.paolorotolo.appintro.ISlideBackgroundColorHolder;
 import com.github.paolorotolo.appintro.ISlidePolicy;
 
 import butterknife.BindView;
@@ -23,19 +28,21 @@ import butterknife.ButterKnife;
  * Description: Register the new user
  */
 
-public class RegisterStepOne extends Fragment implements ISlidePolicy{
+public class RegisterStepOne extends Fragment implements ISlidePolicy, ISlideBackgroundColorHolder{
 
     /*UI views*/
-    @BindView(R.id.signup_email_id) EditText signUpEmail;
-    @BindView(R.id.signup_password_id) EditText signUpPassword;
-    @BindView(R.id.signup_emailtxtInput_id) TextInputLayout signUpEmailTxtInptLayout;
-    @BindView(R.id.signup_pass_txtInput_id) TextInputLayout signUpPassTxtInptLayout;
-    @BindView(R.id.signup_phoneNo_id) EditText signUpPhoneNo;
-    @BindView(R.id.signup_phoneNoTxtInp_id) TextInputLayout signUpPhoneNoTxtIn;
+    @BindView(R.id.registerstep1_form_container) LinearLayout registerStep1Container;
     @BindView(R.id.signup_name_id) EditText signUpName;
+    @BindView(R.id.signup_email_id) EditText signUpEmail;
+    @BindView(R.id.signup_phoneNo_id) EditText signUpPhoneNo;
+    @BindView(R.id.signup_password_id) EditText signUpPassword;
+    @BindView(R.id.signup_retypepassword_id) EditText signUpRetypePassword;
+
     @BindView(R.id.signup_nametxtInput_id) TextInputLayout signUpNameTxtIn;
-    @BindView(R.id.signup_retypepassword_id) EditText retypePassword;
-    @BindView(R.id.signup_retypepass_txtInput_id) TextInputLayout retypePasswordTxtIn;
+    @BindView(R.id.signup_emailtxtInput_id) TextInputLayout signUpEmailTxtInptLayout;
+    @BindView(R.id.signup_phoneNoTxtInp_id) TextInputLayout signUpPhoneNoTxtIn;
+    @BindView(R.id.signup_pass_txtInput_id) TextInputLayout signUpPassTxtInptLayout;
+    @BindView(R.id.signup_retypepass_txtInput_id) TextInputLayout signUpRetypePasswordTxtIn;
 
     public RegisterStepOne(){}
 
@@ -59,6 +66,19 @@ public class RegisterStepOne extends Fragment implements ISlidePolicy{
     /**Notify the user of the unchecked requirements to proceed to next slide*/
     @Override
     public void onUserIllegallyRequestedNextPage() {
+        Toast.makeText(getContext(), R.string.registerstep_slide_policy_error, Toast.LENGTH_SHORT).show();
+    }
 
+    /**overrides the color of the background color in the xml layout*/
+    @Override
+    public int getDefaultBackgroundColor() {
+        return Color.parseColor("#FFDEAD");
+    }
+
+    @Override
+    public void setBackgroundColor(@ColorInt int backgroundColor) {
+        if (registerStep1Container != null) {
+            registerStep1Container.setBackgroundColor(backgroundColor);
+        }
     }
 }
