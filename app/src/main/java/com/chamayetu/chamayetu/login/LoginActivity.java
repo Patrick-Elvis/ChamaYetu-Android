3b954a5609df1ae68277ae992b5bf3a006238835
BrianLusina <lusinabrian@gmail.com>
2016-10-09 19:47:52 +0300

    Removed login form from login activity

--- app/src/main/java/com/chamayetu/chamayetu/login/LoginActivity.java
+++ app/src/main/java/com/chamayetu/chamayetu/login/LoginActivity.java
@@ -6,6 +6,7 @@ import android.annotation.TargetApi;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -76,16 +77,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     @BindView(R.id.cv_container) CardView cardViewContainer;
 
     @BindView(R.id.email) AutoCompleteTextView mEmailView;
-    @BindView(R.id.email_txtInputLayout) TextInputLayout mEmailTxtInpView;
-
     @BindView(R.id.password) EditText mPasswordView;
-    @BindView(R.id.password_txtInputLayout) TextInputLayout mPasswordTxtInpView;
-
     @BindView(R.id.email_sign_in_button)Button mEmailSignInButton;
-    @BindView(R.id.registerBtn)Button mRegisterButton;
+    @BindView(R.id.forgot_password_link) Textview forgotPassword;
+    @BindView(R.id.fab) FloatingActionButton floatingActionButton;
 
-    @BindView(R.id.login_form) View mProgressView;
-    @BindView(R.id.login_progress) View mLoginFormView;
     @BindView(R.id.facebook_login_button) LoginButton facebookLoginBtn;
     @BindView(R.id.google_signin_button) SignInButton googleSignInButton;
     private CallbackManager callbackManager;
@@ -114,7 +110,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         });
 
         mEmailSignInButton.setOnClickListener(view -> attemptLogin());
-        mRegisterButton.setOnClickListener(view -> startActivity(new Intent(this, RegisterSlideContainer.class)));
         callbackManager = CallbackManager.Factory.create();
 
         mAuth = FirebaseAuth.getInstance();
@@ -279,7 +274,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
-            showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
         }
@@ -293,39 +287,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         return password.isEmpty();
     }
 
-    /*** Shows the progress UI and hides the login form.*/
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
-    private void showProgress(final boolean show) {
-        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
-        // for very easy animations. If available, use these APIs to fade-in
-        // the progress spinner.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
-            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
-
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-            mLoginFormView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-                }
-            });
-
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mProgressView.animate().setDuration(shortAnimTime).alpha(
-                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
-                @Override
-                public void onAnimationEnd(Animator animation) {
-                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-                }
-            });
-        } else {
-            // The ViewPropertyAnimator APIs are not available, so simply show
-            // and hide the relevant UI components.
-            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
-            mLoginFormView.setVisibility(show ? View.GONE : View.VISIBLE);
-        }
-    }
 
     @Override
     public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
@@ -432,7 +393,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         @Override
         protected void onPostExecute(final Boolean success) {
             mAuthTask = null;
-            showProgress(false);
 
             // if successful, start main activity
             if (success) {
@@ -447,7 +407,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         @Override
         protected void onCancelled() {
             mAuthTask = null;
-            showProgress(false);
         }
     }
 
