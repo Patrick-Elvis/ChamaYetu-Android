e003afaf04fcf1a5dd0942fa6d81916045257243
BrianLusina <lusinabrian@gmail.com>
2016-10-12 12:09:54 +0300

    Create classes for the full statement and mini statements to the user

---
+++ app/src/main/java/com/chamayetu/chamayetu/statements/FullStatement.java
@@ -0,0 +1,11 @@
+package com.chamayetu.chamayetu.statements;
+
+/**
+ * ChamaYetu
+ * com.chamayetu.chamayetu.statements
+ * Created by lusinabrian on 12/10/16.
+ * Description: Display a full statement to the user
+ */
+
+public class FullStatement {
+}
---
+++ app/src/main/java/com/chamayetu/chamayetu/statements/MiniStatement.java
@@ -0,0 +1,11 @@
+package com.chamayetu.chamayetu.statements;
+
+/**
+ * ChamaYetu
+ * com.chamayetu.chamayetu.statements
+ * Created by lusinabrian on 12/10/16.
+ * Description: Display Mini statement for a defined period to the user
+ */
+
+public class MiniStatement {
+}
