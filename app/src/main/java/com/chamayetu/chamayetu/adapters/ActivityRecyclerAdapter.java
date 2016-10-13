0bb267824fb0bf60caa06b8a8f66f8fdfce7f95b
BrianLusina <lusinabrian@gmail.com>
2016-10-13 15:35:38 +0300

    Create ActivityModel

--- app/src/main/java/com/chamayetu/chamayetu/adapters/ActivityRecyclerAdapter.java
+++ app/src/main/java/com/chamayetu/chamayetu/adapters/ActivityRecyclerAdapter.java
@@ -2,7 +2,15 @@ package com.chamayetu.chamayetu.adapters;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.chamayetu.chamayetu.R;
+import com.chamayetu.chamayetu.models.ActivityModel;
+
+import java.util.List;
 
 /**
  * ChamaYetu
@@ -11,23 +19,57 @@ import android.view.ViewGroup;
  * Description: Adapter for the Activity of the Chama
  */
 
-public class ActivityRecyclerAdapter extends RecyclerView.Adapter<> {
+public class ActivityRecyclerAdapter extends RecyclerView.Adapter<ActivityRecyclerAdapter.ViewHolder> {
     private Context mContext;
-    private List<ActorModel> actorModelList;
-    public int itemLayout;
+    private List<ActivityModel> actitivyModelList;
+    private int itemLayout;
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+    // constructor
+    public ActivityRecyclerAdapter(Context mContext, List<ActivityModel> actitivyModelList, int itemLayout){
+        this.mContext = mContext;
+        this.itemLayout = itemLayout;
+        this.actitivyModelList = actitivyModelList;
+        this.notifyDataSetChanged();
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(itemLayout, parent, false);
+        return new ViewHolder(itemView);
+    }
 
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        ActivityModel activityModel = actitivyModelList.get(position);
+        holder.itemView.setTag(activityModel);
+        holder.bind(activityModel);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView personName, activityName, activityDate,amount;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            personName = (TextView) itemView.findViewById(R.id.chamaactivityperson_name);
+            activityName = (TextView) itemView.findViewById(R.id.chamaactivity_type);
+            activityDate = (TextView)itemView.findViewById(R.id.chamaactivity_date);
+            amount = (TextView)itemView.findViewById(R.id.chamaactivity_amount);
+        }
+        public void bind(ActivityModel activityModel){
+            personName.setText(activityModel.getPerson());
+            activityName.setText(activityModel.getActivityType());
+            activityDate.setText(activityModel.getDate());
+            amount.setText(String.valueOf(activityModel.getAmount()));
+        }
+    }
+
+    public void add(ActivityModel itemModel, int position){
+        actitivyModelList.add(position,itemModel);
+        notifyDataSetChanged();
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return actitivyModelList.size();
     }
 }
--- app/src/main/java/com/chamayetu/chamayetu/models/ActivityModel.java
+++ app/src/main/java/com/chamayetu/chamayetu/models/ActivityModel.java
@@ -1,5 +1,6 @@
 package com.chamayetu.chamayetu.models;
 
+import android.os.Parcel;
 import android.os.Parcelable;
 
 /**
@@ -10,4 +11,89 @@ import android.os.Parcelable;
  */
 
 public class ActivityModel implements Parcelable {
+    private String activityType, person, date;
+    private long amount;
+
+    public ActivityModel(){}
+
+    public ActivityModel(String activityType, String person, String date, long amount) {
+        this.activityType = activityType;
+        this.person = person;
+        this.date = date;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "ActivityModel{" +
+                "activityType='" + activityType + '\'' +
+                ", person='" + person + '\'' +
+                ", date='" + date + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+
+    public String getActivityType() {
+        return activityType;
+    }
+
+    public void setActivityType(String activityType) {
+        this.activityType = activityType;
+    }
+
+    public String getPerson() {
+        return person;
+    }
+
+    public void setPerson(String person) {
+        this.person = person;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.activityType);
+        dest.writeString(this.person);
+        dest.writeString(this.date);
+        dest.writeLong(this.amount);
+    }
+
+    protected ActivityModel(Parcel in) {
+        this.activityType = in.readString();
+        this.person = in.readString();
+        this.date = in.readString();
+        this.amount = in.readLong();
+    }
+
+    public static final Creator<ActivityModel> CREATOR = new Creator<ActivityModel>() {
+        @Override
+        public ActivityModel createFromParcel(Parcel source) {
+            return new ActivityModel(source);
+        }
+
+        @Override
+        public ActivityModel[] newArray(int size) {
+            return new ActivityModel[size];
+        }
+    };
 }
