3a4551a49391379278e85a46f0ab3b2fd410028e
BrianLusina <lusinabrian@gmail.com>
2016-10-13 11:54:32 +0300

    Add nodpi res

---
+++ app/src/main/java/com/chamayetu/chamayetu/graph/MyAxisValueFormatter.java
@@ -0,0 +1,31 @@
+package com.chamayetu.chamayetu.graph;
+
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.formatter.AxisValueFormatter;
+
+import java.text.DecimalFormat;
+
+/**
+ * ChamaYetu
+ * com.chamayetu.chamayetu.graph
+ * Created by lusinabrian on 13/10/16.
+ * Description: formats the axis values of the bar chart
+ */
+
+public class MyAxisValueFormatter implements AxisValueFormatter {
+    private DecimalFormat mFormat;
+
+    public MyAxisValueFormatter() {
+        mFormat = new DecimalFormat("###,###,###,##0.0");
+    }
+
+    @Override
+    public String getFormattedValue(float value, AxisBase axis) {
+        return mFormat.format(value) + " $";
+    }
+
+    @Override
+    public int getDecimalDigits() {
+        return 1;
+    }
+}
--- app/src/main/java/com/chamayetu/chamayetu/graph/StatementBarGraph.java
+++ app/src/main/java/com/chamayetu/chamayetu/graph/StatementBarGraph.java
@@ -3,6 +3,9 @@ package com.chamayetu.chamayetu.graph;
 import android.content.Context;
 
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.highlight.Highlight;
 import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
@@ -34,6 +37,7 @@ public class StatementBarGraph implements OnChartValueSelectedListener{
         mBarChart.setDrawBarShadow(false);
         mBarChart.setDrawValueAboveBar(true);
 
+
         // if more than 60 entries are displayed in the chart, no values will be
         // drawn
         mBarChart.setMaxVisibleValueCount(60);
@@ -43,10 +47,71 @@ public class StatementBarGraph implements OnChartValueSelectedListener{
 
         mBarChart.setDrawGridBackground(false);
         // mBarChart.setDrawYLabels(false);
+        AxisValueFormatter xAxisFormatter = new DayAxisValueFormatter(mBarChart);
+        AxisValueFormatter custom = new MyAxisValueFormatter();
 
-        AxisValueFormatter xAxisFormatter = new DayAxisValueFormatter(mChart);
+        initXAxis(xAxisFormatter);
+        initYAxis(custom);
+        createLegend();
+
+        XYMarkerView mv = new XYMarkerView(this, xAxisFormatter);
+        mv.setChartView(mChart); // For bounds control
+        mChart.setMarker(mv); // Set the marker to the chart
+
+        setData(12, 50);
+
+        // setting data
+        mSeekBarY.setProgress(50);
+        mSeekBarX.setProgress(12);
+
+        mSeekBarY.setOnSeekBarChangeListener(this);
+        mSeekBarX.setOnSeekBarChangeListener(this);
 
     }
+
+    /**Initialize the Legend and create it*/
+    private void createLegend() {
+        Legend l = mBarChart.getLegend();
+        l.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        l.setHorizontalAlignment(Legend.LegendHorizontalAlignment.LEFT);
+        l.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        l.setDrawInside(false);
+        l.setForm(Legend.LegendForm.SQUARE);
+        l.setFormSize(9f);
+        l.setTextSize(11f);
+        l.setXEntrySpace(4f);
+    }
+
+    /*initialize the X-axis of Bar Graph*/
+    private void initXAxis(AxisValueFormatter xAxisFormatter) {
+        /*set the x-axis of the bar chart*/
+        XAxis xAxis = mBarChart.getXAxis();
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setGranularity(1f); // only intervals of 1 day
+        xAxis.setLabelCount(7);
+        xAxis.setValueFormatter(xAxisFormatter);
+    }
+
+    /**initialize the Y Axis of the Bar Graph*/
+    private void initYAxis(AxisValueFormatter custom){
+        YAxis leftAxis = mBarChart.getAxisLeft();
+        leftAxis.setLabelCount(8, false);
+        leftAxis.setValueFormatter(custom);
+        leftAxis.setPosition(YAxis.YAxisLabelPosition.OUTSIDE_CHART);
+        leftAxis.setSpaceTop(15f);
+        leftAxis.setAxisMinValue(0f);
+
+        /*right axis of the bar chart*/
+        YAxis rightAxis = mBarChart.getAxisRight();
+        rightAxis.setDrawGridLines(false);
+        rightAxis.setLabelCount(8, false);
+        rightAxis.setValueFormatter(custom);
+        rightAxis.setSpaceTop(15f);
+        rightAxis.setAxisMinValue(0f); // this replaces setStartAtZero(true)
+    }
+
+
 
     @Override
     public void onValueSelected(Entry e, Highlight h) {
