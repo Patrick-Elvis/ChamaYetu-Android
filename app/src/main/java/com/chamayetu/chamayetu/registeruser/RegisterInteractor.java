56d2376dbb253bfa4b8fc294c48eb29bf86eade2
BrianLusina <lusinabrian@gmail.com>
2016-10-15 10:50:50 +0300

    Add docstrings for registerinteractor

--- app/src/main/java/com/chamayetu/chamayetu/registeruser/RegisterInteractor.java
+++ app/src/main/java/com/chamayetu/chamayetu/registeruser/RegisterInteractor.java
@@ -4,7 +4,6 @@ package com.chamayetu.chamayetu.registeruser;
  * ChamaYetu
  * com.chamayetu.chamayetu.registeruser
  * Created by lusinabrian on 15/10/16.
- * Description:
  * Interactor implementation returns the results or just returns the control to presenter implementation by calling listener methods
  */
 
@@ -23,6 +22,23 @@ public interface RegisterInteractor {
 
     }
 
-    void login(String name, String password, OnRegistrationFinishedListener listener);
+    interface OnRegisterNewChamaFinishedListener{
+        /**Displays errors when the new chama name is already existing*/
+        void onChamaNameError();
+
+    }
+
+    /**Registers a new user taking in the name, password, phone number an a registration listener
+     * @param name name of the ner user
+     * @param password password of the new user
+     * @param phoneNumber Number of the new user
+     * @param listener registration listner*/
+    void registerNewUser(String name, String password, long phoneNumber, OnRegistrationFinishedListener listener);
+
+    /**registers a new chama to the database adding the name and the members
+     * @param chamaName  name of the chama
+     * @param chamaMembers members of the new chama
+     * @param chairPerson the chairperson name, which is the new registering member*/
+    void registerNewChama(String chamaName, String chamaMembers, String chairPerson, OnRegisterNewChamaFinishedListener listener);
 
 }
