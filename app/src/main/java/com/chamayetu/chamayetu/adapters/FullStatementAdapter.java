59127c18cf9ce34400b198abcdd834de24527c52
BrianLusina <lusinabrian@gmail.com>
2016-10-19 08:58:35 +0300

    Create Full statement recycler adapter

--- app/src/main/java/com/chamayetu/chamayetu/adapters/FullStatementAdapter.java
+++ app/src/main/java/com/chamayetu/chamayetu/adapters/FullStatementAdapter.java
@@ -1,5 +1,20 @@
 package com.chamayetu.chamayetu.adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.chamayetu.chamayetu.R;
+import com.chamayetu.chamayetu.models.FullStatementModel;
+
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
+
 /**
  * ChamaYetu
  * com.chamayetu.chamayetu.adapters
@@ -7,6 +22,61 @@ package com.chamayetu.chamayetu.adapters;
  * Description: Adapter handling data received from Statementnode
  */
 
-public class FullStatementAdapter {
+public class FullStatementAdapter extends RecyclerView.Adapter<FullStatementAdapter.ViewHolder> {
+    private Context mContext;
+    private List<FullStatementModel> fullStatementModelList;
+    private int itemLayout;
 
+    // constructor
+    public FullStatementAdapter(Context mContext, List<FullStatementModel> fullStatementModelList, int itemLayout){
+        this.mContext = mContext;
+        this.itemLayout = itemLayout;
+        this.fullStatementModelList = fullStatementModelList;
+        this.notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(itemLayout, parent, false);
+        return new ViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        FullStatementModel fullStatementModel = fullStatementModelList.get(position);
+        holder.itemView.setTag(fullStatementModel);
+        holder.bind(fullStatementModel);
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+        public TextView date, transactor, details,amount;
+        public CircleImageView itemImg;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            date = (TextView) itemView.findViewById(R.id.full_statement_item_date);
+            transactor = (TextView) itemView.findViewById(R.id.full_statement_item_transactor);
+            details = (TextView)itemView.findViewById(R.id.full_statement_item_details);
+            amount = (TextView)itemView.findViewById(R.id.full_statement_item_amount);
+            itemImg = (CircleImageView)itemView.findViewById(R.id.full_statement_item_img);
+        }
+
+        /*bind the views to the models*/
+        public void bind(FullStatementModel fullStatementModel){
+            date.setText(fullStatementModel.getDate());
+            transactor.setText(fullStatementModel.getTransactor());
+            details.setText(fullStatementModel.getDate());
+            amount.setText(String.valueOf(fullStatementModel.getAmount()) + " Ksh" );
+        }
+    }
+
+    public void add(FullStatementModel itemModel, int position){
+        fullStatementModelList.add(position,itemModel);
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return fullStatementModelList.size();
+    }
 }
--- app/src/main/java/com/chamayetu/chamayetu/models/FullStatementModel.java
+++ app/src/main/java/com/chamayetu/chamayetu/models/FullStatementModel.java
@@ -1,5 +1,6 @@
 package com.chamayetu.chamayetu.models;
 
+import android.os.Parcel;
 import android.os.Parcelable;
 
 /**
@@ -10,5 +11,89 @@ import android.os.Parcelable;
  */
 
 public class FullStatementModel implements Parcelable {
+    private String date, transactor,details;
+    private long amount;
 
+    public FullStatementModel(){}
+
+    public FullStatementModel(String date, String transactor, String details, long amount) {
+        this.date = date;
+        this.transactor = transactor;
+        this.details = details;
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "FullStatementModel{" +
+                "date='" + date + '\'' +
+                ", transactor='" + transactor + '\'' +
+                ", details='" + details + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTransactor() {
+        return transactor;
+    }
+
+    public void setTransactor(String transactor) {
+        this.transactor = transactor;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(long amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.date);
+        dest.writeString(this.transactor);
+        dest.writeString(this.details);
+        dest.writeLong(this.amount);
+    }
+
+    private FullStatementModel(Parcel in) {
+        this.date = in.readString();
+        this.transactor = in.readString();
+        this.details = in.readString();
+        this.amount = in.readLong();
+    }
+
+    public static final Creator<FullStatementModel> CREATOR = new Creator<FullStatementModel>() {
+        @Override
+        public FullStatementModel createFromParcel(Parcel source) {
+            return new FullStatementModel(source);
+        }
+
+        @Override
+        public FullStatementModel[] newArray(int size) {
+            return new FullStatementModel[size];
+        }
+    };
 }
