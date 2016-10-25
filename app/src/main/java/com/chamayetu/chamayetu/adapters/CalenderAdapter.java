2e183fc3af5062c3aa89df050e1b08cad4caa975
BrianLusina <lusinabrian@gmail.com>
2016-10-25 09:25:59 +0300

    Add Calender adapter

---
+++ app/src/main/java/com/chamayetu/chamayetu/adapters/CalenderAdapter.java
@@ -0,0 +1,11 @@
+package com.chamayetu.chamayetu.adapters;
+
+/**
+ * ChamaYetu
+ * com.chamayetu.chamayetu.adapters
+ * Created by lusinabrian on 25/10/16.
+ * Description:
+ */
+
+public class CalenderAdapter {
+}
