4217b9cd250084ca24717c225e81e1f8668a13ae
BrianLusina <lusinabrian@gmail.com>
2016-10-25 08:59:54 +0300

    Add Calender layout

--- app/src/main/java/com/chamayetu/chamayetu/calender/CalenderFrag.java
+++ app/src/main/java/com/chamayetu/chamayetu/calender/CalenderFrag.java
@@ -1,6 +1,17 @@
 package com.chamayetu.chamayetu.calender;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.chamayetu.chamayetu.R;
+
+import butterknife.ButterKnife;
 
 /**
  * ChamaYetu
@@ -19,5 +30,57 @@ public class CalenderFrag extends Fragment{
         return calenderFrag;
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.calenderfrag_layout, container, false);
+        ButterKnife.bind(this, rootView);
+
+        return  rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 }
