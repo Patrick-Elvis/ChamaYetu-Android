fe36c69afa7f184c91a0ea1e93931cf46ec12232
BrianLusina <lusinabrian@gmail.com>
2016-10-29 01:10:29 +0300

    modify logininteractor imple

--- app/src/main/java/com/chamayetu/chamayetu/auth/loginchama/LoginChamaInteractorImpl.java
+++ app/src/main/java/com/chamayetu/chamayetu/auth/loginchama/LoginChamaInteractorImpl.java
@@ -2,10 +2,14 @@ package com.chamayetu.chamayetu.auth.loginchama;
 
 import android.content.Context;
 
+import com.chamayetu.chamayetu.R;
 import com.chamayetu.chamayetu.adapters.LoginChamaViewHolder;
 import com.chamayetu.chamayetu.models.LoginChamaModel;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.database.DatabaseReference;
+
+import static com.chamayetu.chamayetu.utils.Contract.CHAMA_GROUPS;
+import static com.chamayetu.chamayetu.utils.Contract.USERS_NODE;
 
 /**
  * ChamaYetu
@@ -14,9 +18,24 @@ import com.google.firebase.database.DatabaseReference;
  * Description:
  */
 
-public class LoginChamaInteractorImpl implements LoginChamaInteractor {
+class LoginChamaInteractorImpl implements LoginChamaInteractor {
     @Override
     public void findItems(String username, Context context, FirebaseRecyclerAdapter<LoginChamaModel, LoginChamaViewHolder> loginChamaRecyAdapter, DatabaseReference mDatabaseReference, OnFinishedListener listener) {
-
+        loginChamaRecyAdapter = new FirebaseRecyclerAdapter<LoginChamaModel, LoginChamaViewHolder>(
+                LoginChamaModel.class,
+                R.layout.chama_login_item_layout,
+                LoginChamaViewHolder.class,
+                mDatabaseReference.child(USERS_NODE).child(CHAMA_GROUPS)
+        ) {
+            @Override
+            protected void populateViewHolder(LoginChamaViewHolder viewHolder,
+                                              LoginChamaModel model, int position) {
+                for(String key: model.getChamaGroups().keySet()){
+                    viewHolder.chamaName.setText(key);
+                }
+            }
+        };
+        //pass on the adapter to the interactor
+        listener.onFinished(loginChamaRecyAdapter);
     }
 }
