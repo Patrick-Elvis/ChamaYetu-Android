bf4e52e2ef5dd2df48f9df74987cfbeda0bf7c54
BrianLusina <lusinabrian@gmail.com>
2016-11-01 11:11:48 +0300

    Add TextImageDrawable to loginChama items

--- app/src/main/java/com/chamayetu/chamayetu/adapters/LoginChamaViewHolder.java
+++ app/src/main/java/com/chamayetu/chamayetu/adapters/LoginChamaViewHolder.java
@@ -1,8 +1,13 @@
 package com.chamayetu.chamayetu.adapters;
 
+import android.graphics.Color;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.chamayetu.chamayetu.R;
 
 /**
@@ -15,13 +20,32 @@ import com.chamayetu.chamayetu.R;
 public class LoginChamaViewHolder extends RecyclerView.ViewHolder{
 
     public TextView chamaName;
+    public ImageView chamaImg;
 
     public LoginChamaViewHolder(View itemView) {
         super(itemView);
         chamaName = (TextView) itemView.findViewById(R.id.chama_login_item_name);
+        chamaImg = (ImageView)itemView.findViewById(R.id.chama_login_item_img);
+
     }
 
     public void bind(String loginChamaModel){
         chamaName.setText(loginChamaModel+ " account");
+        ColorGenerator generator = ColorGenerator.MATERIAL; // or use DEFAULT
+        // generate random color
+        int color1 = generator.getRandomColor();
+
+        TextDrawable drawable = TextDrawable.builder()
+                .beginConfig()
+                    .textColor(Color.WHITE)
+                    .bold()
+                    .toUpperCase()
+                .endConfig()
+                .buildRound(
+                        String.valueOf(loginChamaModel.charAt(0)),
+                        color1
+                );
+
+        chamaImg.setImageDrawable(drawable);
     }
 }
